d76106844abdfcb393e07be83ca6cf17ab03813e
aldoibuado <aldo.ibuado@gmail.com>
2022-10-19 22:31:56 -0700

    added JavaScript to show the current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,2 @@
-var textareaEl = $("<textarea>");
-
-for (var i = 0; i < 9; i++) {
-  $(".container").append(textareaEl).attr("id", i);
-  console.log(i);
-}
+var todayDate = moment().format("dddd, MMM Do YYYY");
+$("#currentDay").html(todayDate);
